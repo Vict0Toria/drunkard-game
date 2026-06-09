442a9258be63f4d489874b49240b9d99e4171642
Vict0Toria <88_honor@mail.ru>
2026-06-09 15:55:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,16 +22,16 @@ private:
 
     string rankToString(int rank)
     {
-        if(rank <= 10)
+        if (rank >= 6 && rank <= 10)
             return to_string(rank);
 
-        if(rank == 11)
+        if (rank == 11)
             return "J";
 
-        if(rank == 12)
+        if (rank == 12)
             return "Q";
 
-        if(rank == 13)
+        if (rank == 13)
             return "K";
 
         return "A";
@@ -39,34 +39,37 @@ private:
 
     string suitToString(int suit)
     {
-        if(suit == 0)
+        if (suit == 0)
             return "♠";
 
-        if(suit == 1)
+        if (suit == 1)
             return "♥";
 
-        if(suit == 2)
+        if (suit == 2)
             return "♦";
 
         return "♣";
     }
 
-    string cardToString(Card c)
+    string cardToString(Card card)
     {
-        return rankToString(c.rank)
-               + suitToString(c.suit);
+        return rankToString(card.rank) +
+               suitToString(card.suit);
     }
 
-    bool beats(Card attack, Card defend)
+    int getNumber()
     {
-        if(attack.suit == defend.suit)
-            return defend.rank > attack.rank;
+        int value;
 
-        if(defend.suit == trumpSuit &&
-           attack.suit != trumpSuit)
-            return true;
+        while (!(cin >> value))
+        {
+            cout << "Ошибка ввода. Введите число: ";
 
-        return false;
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+
+        return value;
     }
 
     void createDeck()
@@ -75,9 +78,9 @@ private:
         player.clear();
         computer.clear();
 
-        for(int suit = 0; suit < 4; suit++)
+        for (int suit = 0; suit < 4; suit++)
         {
-            for(int rank = 6; rank <= 14; rank++)
+            for (int rank = 6; rank <= 14; rank++)
             {
                 deck.push_back({rank, suit});
             }
@@ -88,14 +91,13 @@ private:
             deck.end()
         );
 
-        trumpSuit =
-            deck.back().suit;
+        trumpSuit = deck.back().suit;
     }
 
     void dealCards()
     {
-        while(player.size() < 6 &&
-              !deck.empty())
+        while (player.size() < 6 &&
+               !deck.empty())
         {
             player.push_back(
                 deck.back()
@@ -104,8 +106,8 @@ private:
             deck.pop_back();
         }
 
-        while(computer.size() < 6 &&
-              !deck.empty())
+        while (computer.size() < 6 &&
+               !deck.empty())
         {
             computer.push_back(
                 deck.back()
@@ -115,13 +117,34 @@ private:
         }
     }
 
+    bool beats(Card attack,
+               Card defend)
+    {
+        if (attack.suit ==
+            defend.suit)
+        {
+            return defend.rank >
+                   attack.rank;
+        }
+
+        if (defend.suit ==
+                trumpSuit &&
+            attack.suit !=
+                trumpSuit)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
     void showPlayerCards()
     {
         cout << "\nВаши карты:\n";
 
-        for(size_t i = 0;
-            i < player.size();
-            i++)
+        for (int i = 0;
+             i < player.size();
+             i++)
         {
             cout
                 << i + 1
@@ -134,7 +157,6 @@ private:
     }
 
 public:
-
     void play()
     {
         createDeck();
@@ -148,56 +170,40 @@ public:
                )
             << "\n";
 
-        while(
-            !player.empty() &&
-            !computer.empty()
-        )
+        while (!player.empty() &&
+               !computer.empty())
         {
             showPlayerCards();
 
             int choice;
 
-            while(true)
+            while (true)
             {
                 cout
                     << "\nВыберите карту для атаки: ";
 
-                if(!(cin >> choice))
-                {
-                    cout
-                        << "Ошибка! Введите число.\n";
-
-                    cin.clear();
-
-                    cin.ignore(
-                        10000,
-                        '\n'
-                    );
-
-                    continue;
-                }
+                choice =
+                    getNumber();
 
                 choice--;
 
-                if(choice < 0 ||
-                   choice >=
-                   (int)player.size())
+                if (choice >= 0 &&
+                    choice <
+                        player.size())
                 {
-                    cout
-                        << "Такой карты нет.\n";
-
-                    continue;
+                    break;
                 }
 
-                break;
+                cout
+                    << "Такой карты нет.\n";
             }
 
             Card attack =
                 player[choice];
 
             player.erase(
-                player.begin()
-                + choice
+                player.begin() +
+                choice
             );
 
             cout
@@ -209,21 +215,20 @@ public:
 
             int defendIndex = -1;
 
-            for(size_t i = 0;
-                i < computer.size();
-                i++)
+            for (int i = 0;
+                 i < computer.size();
+                 i++)
             {
-                if(beats(
-                       attack,
-                       computer[i]
-                   ))
+                if (beats(
+                        attack,
+                        computer[i]))
                 {
                     defendIndex = i;
                     break;
                 }
             }
 
-            if(defendIndex == -1)
+            if (defendIndex == -1)
             {
                 cout
                     << "Компьютер не смог отбиться\n";
@@ -234,25 +239,22 @@ public:
             }
             else
             {
-                Card defend =
-                    computer[
-                        defendIndex
-                    ];
-
                 cout
                     << "Компьютер отбился "
                     << cardToString(
-                           defend
+                           computer[
+                               defendIndex
+                           ]
                        )
                     << "\n";
 
                 computer.erase(
-                    computer.begin()
-                    + defendIndex
+                    computer.begin() +
+                    defendIndex
                 );
             }
 
-            if(computer.empty())
+            if (computer.empty())
                 break;
 
             Card computerAttack =
@@ -271,32 +273,15 @@ public:
 
             showPlayerCards();
 
-            int defendChoice;
+            cout
+                << "\nВыберите карту для защиты\n";
+            cout
+                << "(0 - взять карту): ";
 
-            while(true)
-            {
-                cout
-                    << "\nВыберите карту для защиты (0 если взять): ";
+            int defendChoice =
+                getNumber();
 
-                if(!(cin >> defendChoice))
-                {
-                    cout
-                        << "Ошибка! Введите число.\n";
-
-                    cin.clear();
-
-                    cin.ignore(
-                        10000,
-                        '\n'
-                    );
-
-                    continue;
-                }
-
-                break;
-            }
-
-            if(defendChoice == 0)
+            if (defendChoice == 0)
             {
                 cout
                     << "Вы взяли карту\n";
@@ -309,9 +294,9 @@ public:
             {
                 defendChoice--;
 
-                if(defendChoice < 0 ||
-                   defendChoice >=
-                   (int)player.size())
+                if (defendChoice < 0 ||
+                    defendChoice >=
+                        player.size())
                 {
                     cout
                         << "Такой карты нет.\n";
@@ -330,26 +315,25 @@ public:
                             defendChoice
                         ];
 
-                    if(beats(
-                           computerAttack,
-                           defend
-                       ))
+                    if (beats(
+                            computerAttack,
+                            defend))
                     {
                         cout
                             << "Отбились\n";
 
                         player.erase(
-                            player.begin()
-                            + defendChoice
+                            player.begin() +
+                            defendChoice
                         );
                     }
                     else
                     {
                         cout
-                            << "Карта не бьёт.\n";
+                            << "Карта не бьёт\n";
 
                         cout
-                            << "Вы забираете карту.\n";
+                            << "Вы забираете карту\n";
 
                         player.push_back(
                             computerAttack
@@ -364,57 +348,32 @@ public:
                 << "\nКарт в колоде: "
                 << deck.size()
                 << "\n";
-
-            if(deck.empty() &&
-               (player.empty() ||
-                computer.empty()))
-            {
-                break;
-            }
         }
 
-        cout << "\n===== ИГРА ОКОНЧЕНА =====\n";
+        cout
+            << "\n===== ИГРА ОКОНЧЕНА =====\n";
 
-        if(player.empty() &&
-           computer.empty())
-        {
-            cout
-                << "Ничья!\n";
-        }
-        else if(player.empty())
+        if (player.empty())
         {
             cout
                 << "Поздравляем! Вы победили!\n";
         }
-        else if(computer.empty())
+        else if (computer.empty())
         {
             cout
                 << "Победил компьютер!\n";
         }
         else
         {
-            if(player.size() <
-               computer.size())
-            {
-                cout
-                    << "Победили Вы!\n";
-            }
-            else
-            {
-                cout
-                    << "Победил компьютер!\n";
-            }
+            cout
+                << "Ничья!\n";
         }
     }
 };
 
 int main()
 {
-    srand(
-        static_cast<unsigned>(
-            time(nullptr)
-        )
-    );
+    srand(time(nullptr));
 
     DurakGame game;
 
